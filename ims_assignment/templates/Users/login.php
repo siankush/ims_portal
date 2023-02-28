a800f2e0a15834c7c639071a494d5983617e843a
TEQ <siankush@teqmavens.com>
2023-02-28 14:08:24 +0530

    new file

---
+++ ims_assignment/templates/Users/login.php
@@ -0,0 +1,37 @@
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="<?= $baseurl ?>img/images/logo.svg" alt="logo">
+              </div>
+              <h4>Hello! let's get started</h4>
+              <h6 class="font-weight-light">Sign in to continue.</h6>
+              <form class="pt-3">
+              <?php echo $this->Form->create()?>
+                <div class="form-group">
+                <?php echo $this->Form->control("email",['label'=>false,'id'=>'', 'class'=>'form-control form-control-lg','placeholder'=>'Email']); ?>
+
+                </div>
+                <div class="form-group">
+                <?php echo $this->Form->control("password",['label'=>false,'id'=>'', 'class'=>'form-control form-control-lg','placeholder'=>'Password']); ?>
+
+                </div>
+                <div class="mt-3">
+                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN IN</a>
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Don't have an account? <a href="register" class="text-primary">Create</a>
+                </div>
+                <?= $this->Form->end() ?> 
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
---
+++ ims_assignment/templates/layout/dashboardlayout.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
+
+$cakeDescription = 'CakePHP: the rapid development php framework';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+    <!-- <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?> -->
+    <?= $this->Html->css(['vendors/feather/feather','vendors/ti-icons/css/themify-icons','vendors/css/vendor.bundle.base',
+    'vendors/datatables.net-bs4/dataTables.bootstrap4','vendors/ti-icons/css/themify-icons','asset/select.dataTables.min',
+    'asset/vertical-layout-light/style', 'cake']) ?>
+
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+<body>
+    <!-- <nav class="top-nav">
+        <div class="top-nav-title">
+            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
+        </div>
+        <div class="top-nav-links">
+            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
+            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+        </div>
+    </nav> -->
+    <main class="main">
+        <div class="container-fluid p-0">
+            <?= $this->Flash->render() ?>
+            <?= $this->fetch('content') ?>
+        </div>
+    </main>
+    <footer>
+    <?= $this->Html->script('vendors/js/vendor.bundle.base') ?>
+    <?= $this->Html->script('vendors/chart.js/Chart.min') ?>
+    <?= $this->Html->script('vendors/datatables.net/jquery.dataTables.js') ?>
+    <?= $this->Html->script('vendors/datatables.net-bs4/dataTables.bootstrap4.js') ?>
+    <?= $this->Html->script('asset/dataTables.select.min') ?>
+    <?= $this->Html->script('asset/off-canvas.js"') ?>
+    <?= $this->Html->script('asset/hoverable-collapse') ?>
+    <?= $this->Html->script('asset/template') ?>
+    <?= $this->Html->script('asset/settings') ?>
+    <?= $this->Html->script('asset/template') ?>
+    <?= $this->Html->script('asset/todolist.js') ?>
+    <?= $this->Html->script('asset/dashboard.js') ?>
+    <?= $this->Html->script('asset/Chart.roundedBarCharts') ?>
+    </footer>
+
+
+
+    <!-- <script src="vendors/js/vendor.bundle.base.js"></script> -->
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- <script src="vendors/chart.js/Chart.min.js"></script>
+  <script src="vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="js/dataTables.select.min.js"></script> -->
+
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <!-- <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+  <script src="js/settings.js"></script>
+  <script src="js/todolist.js"></script> -->
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <!-- <script src="js/dashboard.js"></script>
+  <script src="js/Chart.roundedBarCharts.js"></script> -->
+</body>
+</html>
